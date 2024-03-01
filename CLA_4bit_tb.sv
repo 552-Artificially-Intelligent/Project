e891757e9eff19a91125e4f5b1c2cb74b85a2b5a
PieCrusted <27575870+PieCrusted@users.noreply.github.com>
2024-03-01 11:01:17 -0600

    Added 4 bit CLA and CLA_tb
    image of test is up on discord

---
+++ CLA_4bit_tb.sv
@@ -0,0 +1,39 @@
+module CLA_4bit_tb();
+
+reg [3:0] A, B;
+reg Cin;
+
+wire [3:0] Sum;
+wire Cout;
+
+CLA_4bit iDUT(.A(A), .B(B), .Cin(Cin), .Sum(Sum), .Cout(Cout));
+
+reg [4:0] expectedSum;
+
+initial begin
+
+	A = 4'b0000;
+	B = 4'b0000;
+	Cin = 0;
+	#5
+
+	for (int i = 0; i < 32; i++) begin
+		A = (i % 2 == 0) ? A + 1 : A;
+		B = (i % 2 == 1) ? B + 1 : B;
+		Cin = (i % 3 == 0) ? 1 : 0;
+
+		expectedSum = A + B + Cin;
+		#5
+
+		if ({Cout, Sum} != expectedSum) begin
+			$display("Failed Adding: ", A, B, "Expected: ", expectedSum);
+		end
+	end
+
+	$display("Everything good!");
+	$stop();
+
+end
+
+
+endmodule
