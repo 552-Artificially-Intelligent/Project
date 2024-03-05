fd9603b360a113d507752331653ac160eacf5af4
Rusheel <rusheel@live.com>
2024-03-05 16:53:15 -0600

    double flopping pc?

--- PC.v
+++ PC.v
@@ -6,19 +6,30 @@ module PC(
     output [15:0] PC
 );
 
-wire [15:0] internalPC, blank;
+wire [15:0] internalPC1, blank1, internalPC2, blank2;
 // Register(clk, rst, D, WriteReg, ReadEnable1, ReadEnable2, Bitline1, Bitline2);
 Register reg0(
     .D(next),
     .WriteReg(en),
-    .clk(clk),
+    .clk(~clk),
     .ReadEnable1(1'b1),
     .rst(~rst_n),
-    .Bitline1(internalPC),
+    .Bitline1(internalPC1),
     .ReadEnable2(1'b0),
     .Bitline2(blank)
 );
 
-assign PC = ~rst_n ? 16'h0000 : internalPC;
+Register reg1(
+    .D(internalPC1),
+    .WriteReg(en),
+    .clk(clk),
+    .ReadEnable1(1'b1),
+    .rst(~rst_n),
+    .Bitline1(internalPC2),
+    .ReadEnable2(1'b0),
+    .Bitline2(blank2)
+);
+
+assign PC = ~rst_n ? 16'h0000 : internalPC2;
 // clk, rst, D, WriteReg, ReadEnable1, ReadEnable2, Bitline1
 endmodule
