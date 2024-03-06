da0ae9ed657ded6d50751b1b9e33d485a82b830d
PieCrusted <27575870+PieCrusted@users.noreply.github.com>
2024-03-05 18:45:41 -0600

    slight shifter change

--- Shifter.sv
+++ Shifter.sv
@@ -9,7 +9,7 @@ output [15:0] Shift_Out; 	// Shifted output data
 wire [15:0] lbit0, lbit1, lbit2, lbit3, rbit0, rbit1, rbit2, rbit3;
 // Idea/Inspiration is to use software case statements, where if there's no breaks
 // it can cascade down and run each case block after it
-assign lbit0 = Shift_Val[0] ? Shift_In << 1 : Shift_In[15:0];
+assign lbit0 = Shift_Val[0] ? Shift_In << 1 : Shift_In;
 assign lbit1 = Shift_Val[1] ? lbit0 << 2 : lbit0;
 assign lbit2 = Shift_Val[2] ? lbit1 << 4 : lbit1;
 assign lbit2 = Shift_Val[3] ? lbit1 << 8 : lbit2;
