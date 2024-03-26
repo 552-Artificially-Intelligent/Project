5f6e484acf108f3f941f9e40abc997aecda3295a
PieCrusted <27575870+PieCrusted@users.noreply.github.com>
2024-03-26 08:41:25 -0500

    ROR now properly works as intended

--- ROR.sv
+++ ROR.sv
@@ -10,13 +10,17 @@ output [15:0] Shift_Out; 	// Shifted output data
 
 wire [15:0] rorbit0, rorbit1, rorbit2, rorbit3;
 
+// assign rorbit0 = Shift_Val[0] ? {Shift_In[0], Shift_In[15:1]} : Shift_In[15:0];
+// assign rorbit1 = Shift_Val[1] ? {rorbit0[1], rorbit0[0], rorbit0[15:2]} : rorbit0;
+// assign rorbit2 = Shift_Val[2] ? {rorbit1[3], rorbit1[2], rorbit1[1], rorbit1[0], 
+// 								rorbit1[15:4]} : rorbit1;
+// assign rorbit3 = Shift_Val[3] ? {rorbit2[7], rorbit2[6], rorbit2[5], rorbit2[4],
+// 								rorbit2[3], rorbit2[2], rorbit2[1], rorbit2[0],
+// 								rorbit2[15:8]} : rorbit2;
 assign rorbit0 = Shift_Val[0] ? {Shift_In[0], Shift_In[15:1]} : Shift_In[15:0];
-assign rorbit1 = Shift_Val[1] ? {rorbit0[1], rorbit0[0], rorbit0[15:2]} : rorbit0;
-assign rorbit2 = Shift_Val[2] ? {rorbit0[3], rorbit0[2], rorbit0[1], rorbit0[0], 
-								rorbit1[15:4]} : rorbit1;
-assign rorbit3 = Shift_Val[3] ? {rorbit0[7], rorbit0[6], rorbit0[5], rorbit0[4],
-								rorbit0[3], rorbit0[2], rorbit0[1], rorbit0[0],
-								rorbit2[15:8]} : rorbit2;
+assign rorbit1 = Shift_Val[1] ? {rorbit0[1:0], rorbit0[15:2]} : rorbit0;
+assign rorbit2 = Shift_Val[2] ? {rorbit1[3:0], rorbit1[15:4]} : rorbit1;
+assign rorbit3 = Shift_Val[3] ? {rorbit2[7:0], rorbit2[15:8]} : rorbit2;
 
 assign Shift_Out = rorbit3;
 
