ef1299c3d085654dec1a5ca25c0dc1e43d710277
PieCrusted <27575870+PieCrusted@users.noreply.github.com>
2024-04-04 07:07:49 -0500

    tiny changes

---
+++ Phase2/Forwarding_Unit.sv
@@ -0,0 +1,14 @@
+module Forwarding_Unit(
+	X_M_RegWrite, X_M_MemWrite,
+	M_W_RegWrite, 
+
+
+
+);
+
+
+
+
+
+
+endmodule
--- Phase2/M_W_Flops.sv
+++ Phase2/M_W_Flops.sv
@@ -1,5 +1,7 @@
 module M_W_Flops(clk, rst, en,);
 
+input clk, rst, en, 
+
 // Data
 
 
