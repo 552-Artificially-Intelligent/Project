6b5ae4015466cd0bfb8258fb57dad60294d47a1b
Joe Ciminski <joe.ciminski@gmail.com>
2024-04-07 15:12:34 -0500

    Progress towards HLT Reqs

--- Phase2/F_D_Flops.sv
+++ Phase2/F_D_Flops.sv
@@ -1,6 +1,7 @@
 module F_D_Flops(
-	clk, rst, wen, instruction_in, oldPC_in, newPC_in, 
-	instruction_out, oldPC_out, newPC_out
+	clk, rst, instruction_in, oldPC_in, newPC_in, 
+	instruction_out, oldPC_out, newPC_out,
+	stopPC
 );
 
 // Currently have 2 different PCs an old and a new
@@ -10,9 +11,17 @@ module F_D_Flops(
 // the FLAG registers should be in the Decode stage, and it will be affected by
 // the Execute stage.
 
-input clk, rst, wen;
-input [15:0] instruction_in, oldPC_in, newPC_in;
-output [15:0] instruction_out, oldPC_out, newPC_out;
+input clk, rst;
+input[3:0] instruction_in;
+input [15:0] oldPC_in, newPC_in;
+output[3:0] instruction_out;
+output [15:0] oldPC_out, newPC_out;
+output stopPC;
+logic currentHalt, stopWrite;
+
+// Halt detection
+assign currentHalt = (instruction_in == 4'b1111);
+BitReg currentlyHalted(.clk(clk), .rst(rst), .wen(wen), .D(currentHalt), .Q(stopPC));
 
 // Use registers for these values since requires 16 bits
 Register reg_inst(.clk(clk), .rst(rst), .WriteReg(wen), .D(instruction_in), 
@@ -26,6 +35,7 @@ Register reg_oldPC(.clk(clk), .rst(rst), .WriteReg(wen), .D(oldPC_in),
 Register reg_newPC(.clk(clk), .rst(rst), .WriteReg(wen), .D(newPC_in), 
 	.ReadEnable1(1'b1), .ReadEnable2(1'b0), 
 	.Bitline1(newPC_out), .Bitline2());
+
 
 
 endmodule
